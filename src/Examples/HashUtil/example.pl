96b87118f5123d9a77665ff4854f28dc320d39b2
zzApotheosis <zzApotheosis@gmail.com>
2021-09-05 11:18:10 -0500

    Minor change to HashUtil example
    Signed-off-by: zzApotheosis <zzApotheosis@gmail.com>

--- src/Examples/HashUtil/example.pl
+++ src/Examples/HashUtil/example.pl
@@ -5,6 +5,8 @@ use warnings;
 use lib "../..";
 use Data::Integrity::HashUtil;
 
+unlink("test.json") if -e "test.json";
+
 my $h = HashUtil->new();
 #$h->hash_walk(algo => "sha512", basedir => "/etc"); # This can be used to walk an entire directory and digest each file in it. The resulting JSON data must be written using $h->write_data()
 $h->add_file("testfile.txt");
