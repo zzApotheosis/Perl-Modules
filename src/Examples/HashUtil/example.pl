1ef3f86d98aea5460a231b251f07b02420386330
zzApotheosis <zzApotheosis@gmail.com>
2021-09-11 16:05:32 -0500

    Add verbose argument to some HashUtil functions
    Also added check for MSWin32 operating system name to prevent ANSI
    Colors from printing on Windows command prompts
    
    Signed-off-by: zzApotheosis <zzApotheosis@gmail.com>

--- src/Examples/HashUtil/example.pl
+++ src/Examples/HashUtil/example.pl
@@ -10,7 +10,7 @@ unlink("test.json") if -e "test.json";
 my $h = HashUtil->new();
 #$h->hash_walk(algo => "sha512", basedir => "/etc"); # This can be used to walk an entire directory and digest each file in it. The resulting JSON data must be written using $h->write_data()
 $h->add_file("testfile.txt");
-$h->walk(basedir => "../..");
+$h->walk(basedir => "../..", verbose => 1);
 $h->digest(algo => "sha512");
 $h->write_data(outfile => "test.json");
 
